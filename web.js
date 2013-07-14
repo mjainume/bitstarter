e6e8a977a8ebfb6c881fd1d6caa9b1bc0f47098f
Mohit Jain <mohitiitroorkee@gmail.com>
2013-07-14 16:31:59 +0000

    Web.js updated with buffer.length and size of index.html

--- web.js
+++ web.js
@@ -2,10 +2,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 var fs = require('fs');
 var string = fs.readFileSync('index.html',"utf8");
-var buffer = new Buffer(256);
+var buffer = new Buffer(2485);
 buffer.write(string,"utf-8");
 app.get('/', function(request, response) {
-  response.send(buffer.toString('utf-8'));
+  response.send(buffer.toString('utf-8',0,buffer.length));
 });
 
 var port = process.env.PORT || 5000;
