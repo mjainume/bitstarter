e6e33f2bbadbb35eb6e42d6e7de8f5f2abfc552a
Mohit Jain <mohitiitroorkee@gmail.com>
2013-07-14 10:32:08 +0000

    Updated web.js to include message from index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
-
+var fs = require('fs');
+var string = fs.readFileSync('index.html',"utf8");
+var buffer = new Buffer(256);
+buffer.write(string,"utf-8");
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer.toString('utf-8',0,27));
 });
 
 var port = process.env.PORT || 5000;
