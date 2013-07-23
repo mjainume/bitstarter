9fa2257c4b376377f30a3c3655c4e6ea7f5e7324
Mohit Jain <mohitiitroorkee@gmail.com>
2013-07-23 22:58:26 +0000

    HW4 Part 1 done

--- web.js
+++ web.js
@@ -2,13 +2,13 @@ var express = require('express');
 var app = express.createServer(express.logger());
 var fs = require('fs');
 var string = fs.readFileSync('index.html',"utf8");
-var buffer = new Buffer(2485);
+var buffer = new Buffer(11201);
 buffer.write(string,"utf-8");
 app.get('/', function(request, response) {
   response.send(buffer.toString('utf-8',0,buffer.length));
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
