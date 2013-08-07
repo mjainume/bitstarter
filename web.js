638a3a30cc08d92a3fdf7bface86c2030d43b28d
Mohit Jain <mohitiitroorkee@gmail.com>
2013-08-07 18:08:49 +0000

    Edited product des in index.html

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 var fs = require('fs');
 var string = fs.readFileSync('index.html',"utf8");
-var buffer = new Buffer(11201);
+var buffer = new Buffer(10822);
 buffer.write(string,"utf-8");
 app.get('/', function(request, response) {
   response.send(buffer.toString('utf-8',0,buffer.length));
